ace92835b0d3d35c33580f2908675f9908502e9b
Thang Tuấn Phát <Saladegg24@gmail.com>
2025-05-04 14:04:26 +0700

    project init

---
+++ index.ts
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+app.use(express.json());
