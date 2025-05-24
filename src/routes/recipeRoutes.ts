c0095db53975bf3dff9a0d098213c4d307918a94
Tran Duy Loc <loctd08092005@gmail.com>
2025-05-24 11:53:20 +0700

    :refactor(recipe): bridge endpoint commnet and rating

--- src/routes/recipeRoutes.ts
+++ src/routes/recipeRoutes.ts
@@ -4,6 +4,7 @@ import * as authMiddleware from '../middleware/authMiddleware';
 import supabase from '../config/supabase';
 import { RequestWithUser } from '../types';
 import { handleRecipeCreationImage, handleRecipeImageErrors } from '../middleware/fileUpload';
+import * as recipeControllerBridge from '../controllers/recipeControllerBridge';
 
 const router = express.Router();
 
@@ -13,12 +14,18 @@ router.get('/get-recipe-latest', authMiddleware.protect, recipeController.getLat
 router.get('/get-recipe-id', authMiddleware.protect, recipeController.getRecipeById);
 router.get('/get-recipe-author', authMiddleware.protect, recipeController.getRecipesByAuthor);
 router.get('/get-random-recipe', authMiddleware.protect, recipeController.getRandomRecipes);
-router.patch('/comment-recipe', authMiddleware.protect, recipeController.commentRecipe);
-router.get('/get-recipe-comments', authMiddleware.protect, recipeController.getCommentsRecipe);
-router.patch('/rating-recipe', authMiddleware.protect, recipeController.ratingRecipe);
-router.get('/get-recipe-rating', authMiddleware.protect, recipeController.getRatingRecipe);
-router.patch('/like-comment', authMiddleware.protect, recipeController.likeComment);
-router.patch('/dislike-comment', authMiddleware.protect, recipeController.dislikeComment);
+// router.patch('/comment-recipe', authMiddleware.protect, recipeController.commentRecipe);
+// router.get('/get-recipe-comments', authMiddleware.protect, recipeController.getCommentsRecipe);
+// router.patch('/rating-recipe', authMiddleware.protect, recipeController.ratingRecipe);
+// router.get('/get-recipe-rating', authMiddleware.protect, recipeController.getRatingRecipe);
+// router.patch('/like-comment', authMiddleware.protect, recipeController.likeComment);
+// router.patch('/dislike-comment', authMiddleware.protect, recipeController.dislikeComment);
+router.patch('/comment-recipe', authMiddleware.protect, recipeControllerBridge.commentRecipe);
+router.get('/get-recipe-comments', authMiddleware.protect, recipeControllerBridge.getCommentsRecipe);
+router.patch('/rating-recipe', authMiddleware.protect, recipeControllerBridge.ratingRecipe);
+router.get('/get-recipe-rating', authMiddleware.protect, recipeControllerBridge.getRatingRecipe);
+router.patch('/like-comment', authMiddleware.protect, recipeControllerBridge.likeComment);
+router.patch('/dislike-comment', authMiddleware.protect, recipeControllerBridge.dislikeComment);
 router.post('/upload-image', authMiddleware.protect, handleRecipeImageErrors ,recipeController.uploadRecipeImage);
 
 export default router;
